fa4b02a71308ff018afa506b848cfd8d44120dfd
Ignacio Mieites <ia.mieites@gmail.com>
2016-10-28 17:51:31 -0300

    el app component ahora usa inline template y styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+    <h1>
+      {{title}}
+    </h1>
+    `,
+  styles:[`
+    h1 {
+    text-align: center
+    }
+`]
 })
 export class AppComponent {
   title = 'NG2 Github Fetch';
